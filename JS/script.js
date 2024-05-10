64ed071d388cb627ed17d999423280d5ceba97a7
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-05-10 19:33:47 +0200

    Aggiunto calcolo sconti e prezzo finale con stampa in console

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,21 @@
-const button = document.querySelector('button');
 
-button.addEventListener('click', function(){
+
+document.querySelector('button').addEventListener('click', function(){
     let userAge = document.getElementById('age');
     const age = Number.parseFloat (userAge.value);
     let userKm = document.getElementById('km');
     const km = Number.parseFloat (userKm.value);
-    console.log(km, age);
+    let sconto = '';
+    let priceKm = km * 0.267113;
+    if(age < 21){
+        sconto = 24.552 / 100
+    }
+    else if(age > 63){
+        sconto = 37.893 / 100
+    }
+    else{
+        sconto = 0
+    }
+    finalPrice = priceKm - (priceKm * sconto);
+    console.log(finalPrice);
 })
